fe68df345ce93c185bb71bd0f6bc6a14f2ac20a1
Iván Dimitri Novomiast Dieguez <idimnovdie1602@protonmail.com>
2023-09-12 18:27:24 +0200

    Add files via upload

---
+++ libft/libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ivnovomi <ivnovomi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 16:28:27 by ivnovomi          #+#    #+#             */
+/*   Updated: 2023/09/12 17:30:42 by ivnovomi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int ft_isprint(int c);
+int	ft_strlen(char *str);
+
+#endif
